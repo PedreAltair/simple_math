513775c4cb3797adee8e971ead988afb84933ee0
Alessio Pedretti <marzioboris@gmail.com>
2025-02-27 13:26:57 +0100

    Version 0.1.0 of simplemath program

---
+++ simplemath.c
@@ -0,0 +1,10 @@
+/** \file simplemath.c
+ *  \brief Definition of the functions
+ *  \author Alessio Pedretti
+ */
+
+#include "simplemath.h"
+
+float mean(int a, int b) {
+    return (a+b)/2;
+}
---
+++ simplemath.h
@@ -0,0 +1,13 @@
+/** \file simplemath.h
+ *  \brief A toy example of math library
+ *  \author Alessio Pedretti
+ */
+
+#define PARAM 4
+
+#ifndef _SIMPLEMATH_
+#define _SIMPLEMATH_
+
+float mean(int a, int b);
+
+#endif
