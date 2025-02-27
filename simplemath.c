c70f332b03af27724381bffc56f94942006cb9aa
Alessio Pedretti <marzioboris@gmail.com>
2025-02-27 14:56:41 +0100

    Version 0.3.0 of simplemath program

--- simplemath.c
+++ simplemath.c
@@ -10,6 +10,14 @@ float mean(int a, int b) {
 	return (a+b)/2;
 }
 
+cx_float_t cx_mean(cx_int_t a, cx_int_t b) {
+	cx_float_t average;
+	average.re = (a.re + b.re) / 2;
+	average.imm = (a.imm + b.imm) / 2;
+
+	return average;
+}
+
 int max(int a, int b) {
 	if (a > b) {
 		return a;
